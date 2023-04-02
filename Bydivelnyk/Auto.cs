f0618f430a693041eea1a1b9704d932dffafeab5
VladLAS <vlad.bigma2004@gmail.com>
2023-04-02 16:38:40 +0300

    Initial commit

---
+++ Bydivelnyk/Auto.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bydivelnyk
+{
+    public interface Builder
+    {
+        string Info();
+        void set_radio(string r);
+        void set_tone(int t);
+        void set_capacity(double c);
+        void set_engine(string e);
+    }
+    //Director
+   public abstract class Auto : Builder
+    {
+        public Auto()
+        {
+
+        }
+        public Auto(Auto a)
+        {
+            this.engine = a.engine;
+            this.type_of_radio = a.type_of_radio;
+            this.tone = a.tone;
+            this.tank_capacity = a.tank_capacity;
+        }
+       public abstract Auto Clone();
+        public string Info() 
+        {
+            return "Версія: " + version + "\nТип радіо: " + type_of_radio + "\nРівень затонованості скла: " 
+                + tone + "\nОб'єм бензобаку: " + tank_capacity + "Litr.\nТип двигуна: " + engine;
+        }
+        protected string version = "";
+        protected string type_of_radio = "";
+        protected int tone = 1;
+        protected double tank_capacity = 1;
+        protected string engine = "";
+        public void set_radio(string r)
+        {
+            type_of_radio = r;
+        }
+        public void set_tone(int t)
+        {
+            tone = t;
+        }
+        public void set_capacity(double c)
+        {
+            tank_capacity = c;
+        }
+        public void set_engine(string e)
+        {
+            engine = e;
+        }
+    }
+    public class Auto1 : Auto
+    {
+        public Auto1()
+        {
+            version = "E153-42";
+        }
+        public Auto1(Auto aa): base(aa)
+        {
+            version = "D64-88";
+        }
+        public override Auto Clone()
+        {
+            return new Auto1(this);
+        }
+    }
+    public class Auto2 : Auto
+    {
+        public Auto2()
+        {
+            version = "E153-43";
+        }
+        public Auto2(Auto aa) : base(aa)
+        {
+            version = "T72-108";
+        }
+        public override Auto Clone()
+        {
+            return new Auto2(this);
+        }
+    }
+    public class Auto3 : Auto
+    {
+        public Auto3()
+        {
+            version = "E153-44";
+        }
+        public Auto3(Auto aa) : base(aa)
+        {
+            version = "B7R-420-Ultra";
+        }
+        public override Auto Clone()
+        {
+            return new Auto2(this);
+        }
+    }
+}
---
+++ Bydivelnyk/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bydivelnyk
+{
+    public partial class Form1 : Form
+    {
+        List<Auto> AutoPark = new List<Auto>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AutoPark.Add(new Auto1());
+            comboBox1.Items.Add("BMW");   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AutoPark.Add(new Auto2());
+            comboBox1.Items.Add("WMB");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AutoPark.Add(new Auto3());
+            comboBox1.Items.Add("MWB");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex >= 0)
+            AutoPark.Add(AutoPark[comboBox1.SelectedIndex].Clone());
+            comboBox1.Items.Add(comboBox1.Items[comboBox1.SelectedIndex].ToString()+ "Clone");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                AutoPark[comboBox1.SelectedIndex].set_radio(textBox1.Text);
+            }
+            if (textBox2.Text != "")
+            {
+                AutoPark[comboBox1.SelectedIndex].set_tone(Convert.ToInt32(textBox2.Text));
+            }
+            if (textBox3.Text != "")
+            {
+                AutoPark[comboBox1.SelectedIndex].set_capacity(Convert.ToDouble(textBox3.Text));
+            }
+            if (textBox4.Text != "")
+            {
+                AutoPark[comboBox1.SelectedIndex].set_engine(textBox4.Text);
+            }
+            if (comboBox1.SelectedIndex >= 0)
+                label1.Text = AutoPark[comboBox1.SelectedIndex].Info();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex >= 0)
+            label1.Text = AutoPark[comboBox1.SelectedIndex].Info();
+        }
+    }
+}
